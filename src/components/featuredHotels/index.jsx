e099695bdc9336b1aa10c2d4b9967791fa010093
Aravind104 <ontagodiaravind@gmail.com>
2023-02-14 12:18:41 +0530

    feat(components):add featured hotel and property list component

---
+++ src/components/featuredHotels/index.jsx
@@ -0,0 +1,66 @@
+import "./featuredHotels.css";
+export const FeaturedHotelsComponent = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/187855604.webp?k=bbb45aa5b540c7608ea3af52d92b95a215df9af831dd3ae0e4c4cce501e28b1b&o=&s=1"
+          alt="fhImg"
+          className="fpImg"
+        />
+        <span className="fpName">Wooden House</span>
+        <span className="fpCity">Bangalore</span>
+        <span className="fpPrice">$1200</span>
+        <div className="fpRating">
+          <button>9.0</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/187855604.webp?k=bbb45aa5b540c7608ea3af52d92b95a215df9af831dd3ae0e4c4cce501e28b1b&o=&s=1"
+          alt="fhImg"
+          className="fpImg"
+        />
+        <span className="fpName">Wooden House</span>
+        <span className="fpCity">Bangalore</span>
+        <span className="fpPrice">$1200</span>
+        <div className="fpRating">
+          <button>9.0</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/187855604.webp?k=bbb45aa5b540c7608ea3af52d92b95a215df9af831dd3ae0e4c4cce501e28b1b&o=&s=1"
+          alt="fhImg"
+          className="fpImg"
+        />
+        <span className="fpName">Wooden House</span>
+        <span className="fpCity">Bangalore</span>
+        <span className="fpPrice">$1200</span>
+        <div className="fpRating">
+          <button>9.0</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/187855604.webp?k=bbb45aa5b540c7608ea3af52d92b95a215df9af831dd3ae0e4c4cce501e28b1b&o=&s=1"
+          alt="fhImg"
+          className="fpImg"
+        />
+        <span className="fpName">Wooden House</span>
+        <span className="fpCity">Bangalore</span>
+        <span className="fpPrice">$1200</span>
+        <div className="fpRating">
+          <button>9.0</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,8 @@
 import { FeaturedComponent } from "../../components/featured";
+import { FeaturedHotelsComponent } from "../../components/featuredHotels";
 import { HeaderComponent } from "../../components/header";
 import { NavbarComponent } from "../../components/navbar";
+import { PropertyListComponent } from "../../components/propertyList";
 import "./home.css";
 export const HomePage = () => {
   return (
@@ -9,6 +11,10 @@ export const HomePage = () => {
       <HeaderComponent />
       <div className="homeContainer">
         <FeaturedComponent />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyListComponent />
+        <h1 className="homeTitle">Stay at our top unique properties</h1>
+        <FeaturedHotelsComponent />
       </div>
     </div>
   );
