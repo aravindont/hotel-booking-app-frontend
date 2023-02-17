a6bfef9ec72e01da19f2796478a9904c8e98531a
Aravind104 <ontagodiaravind@gmail.com>
2023-02-17 17:36:27 +0530

    feat(propery list):fetch the count of properties based on their types from database using API

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 export const NavbarComponent = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Hotel Booking</span>
+        <Link to={"/"}>
+          <span className="logo">Hotel Booking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -5,18 +5,17 @@ const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get(url);
-        setData(res.data);
-      } catch (err) {
-        setError(err);
-      }
-      setLoading(false);
-    };
     fetchData();
   }, [url]);
 
